bf5720a0de873b87e3bc2d1910930bccbf239bb4
Sun Haozhe <275940200@qq.com>
2018-01-25 19:41:24 +0100

    Update Mesh.h

--- IGRV_src/Mesh.h
+++ IGRV_src/Mesh.h
@@ -19,15 +19,18 @@
 
 struct UGridCell{
   Vec3f meanPosition;
+  Vec3f meanNormal;
   unsigned int count;
   std::set<unsigned int> indices;
 
-  UGridCell(): meanPosition(0.0f, 0.0f, 0.0f), count(0) {}
+  UGridCell(): meanPosition(0.0f, 0.0f, 0.0f), meanNormal(0.0f, 0.0f, 0.0f), count(0) {}
 
-  void addNewVertex(unsigned int index, Vec3f new_vertex){
+  void addNewVertex(unsigned int index, Vec3f new_vertex, Vec3f new_normal){
     meanPosition = meanPosition * count + new_vertex;
+    meanNormal = meanNormal * count + new_normal;
     count++;
     meanPosition /= count;
+    meanNormal /= count;
     indices.insert(index);
   }
 };
@@ -82,6 +85,9 @@ public:
     // OCS simplification
     void simplify(unsigned int resolution);
 
+    //Loop subdivision
+    void subdivide();
+
 
 private:
     std::vector<Vec3f> m_positions;
@@ -95,6 +101,10 @@ private:
     void make_cubes(float & x_scale, float & y_scale, float & z_scale, unsigned int resolution, Vec3f & min_p);
     void push_vertices_into_cubes(UGrid & grid, unsigned int resolution,
       float x_scale, float y_scale, float z_scale, std::vector<unsigned int> & index_map, Vec3f & min_p);
-    void calculate_new_positions(std::vector<Vec3f> & new_positions, UGrid & grid);
+    void calculate_new_positions(std::vector<Vec3f> & new_positions, std::vector<Vec3f> & new_normals, UGrid & grid);
     void reindex(const std::vector<unsigned int> & index_map, std::vector<Triangle> & new_triangles);
+    float getAlpha(unsigned int n){ return (1.0f / 64.0f) * (40.0f - pow( (3.0f + 2.0f * cos(2 * M_PI / n) ), 2 ) ); }
+    void addOddVertices(std::map<std::pair<int, int>, Vec3f> & oddVertices, std::map<std::pair<int, int>, unsigned int> & oddVertexIndices, std::vector<Vec3f> & new_positions);
+    void updateEvenVertices(std::vector<unsigned int> & valence, std::vector<Vec3f> & new_positions);
+    void reindex_subdivision(std::map<std::pair<int, int>, unsigned int> & oddVertexIndices, std::vector<Triangle> & new_triangles);
 };
